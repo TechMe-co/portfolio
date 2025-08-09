2420a4efafb4e739f3180d7db8447e61306f5b5d
Sadiya Gambo <prettysmiler6@gmail.com>
2025-08-09 12:03:00 -0400

    updated pp

--- my-portfolio/Uber Website Recreate/app.js
+++
@@ -1,8 +0,0 @@
-function checkNationality() {
-    const isNigerian = confirm("Are you Nigerian");
-    if(!isNigerian) {
-        alert("Access denied. This services is only available in Nigeria.");
-
-        window.location.href = "https://google.com";
-    }
-}
